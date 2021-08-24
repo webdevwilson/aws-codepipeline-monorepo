8a7577006cf561b3361cd0cf626ad831dae9ae1c
Kerry Wilson <kerry@allthingswilson.com>
2021-08-23 19:45:52 -0500

    Deserialize the push event

--- infra/src/cdk/lambda/WebhookFn.java
+++ infra/src/cdk/lambda/WebhookFn.java
@@ -7,6 +7,7 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayV2ProxyResponseEve
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.kohsuke.github.GHCommit;
+import org.kohsuke.github.GHEventPayload;
 
 public class WebhookFn implements RequestHandler<APIGatewayV2ProxyRequestEvent, APIGatewayV2ProxyResponseEvent> {
 
@@ -15,10 +16,10 @@ public class WebhookFn implements RequestHandler<APIGatewayV2ProxyRequestEvent, 
         System.out.println("Received event: " + evt);
         final String body = evt.getBody();
         System.out.println("request body: " + body);
-//        final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//        final GHCommit commit = gson.fromJson(body, GHCommit.class);
-//        System.out.println("commit id:" + commit.getSHA1());
-//
+        final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        final GHEventPayload.Push pushEvent = gson.fromJson(body, GHEventPayload.Push.class);
+        System.out.println("commit id:" + pushEvent.getHead());
+
         final APIGatewayV2ProxyResponseEvent resp = new APIGatewayV2ProxyResponseEvent();
         resp.setStatusCode(200);
         resp.setBody("OK");
